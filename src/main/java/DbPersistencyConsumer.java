f801c5dec74b7b236d855e173053c0a17ef2bcdc
Alessandro Dalfovo <alessandro.dalfovo@paros.srl>
2017-09-20 14:36:37 +0200

    Redeliver failed messages to dlq

--- src/main/java/DbPersistencyConsumer.java
+++ src/main/java/DbPersistencyConsumer.java
@@ -2,9 +2,12 @@ import com.rabbitmq.client.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
+import static com.rabbitmq.client.BuiltinExchangeType.FANOUT;
 import static com.rabbitmq.client.BuiltinExchangeType.TOPIC;
 import static java.lang.Thread.sleep;
 
@@ -20,19 +23,29 @@ public class DbPersistencyConsumer {
         final Channel channel = connection.createChannel();
 
         channel.exchangeDeclare("mumble", TOPIC);
-        channel.queueDeclare("db", true, false, false, null);
-        channel.queueBind("db", "mumble", "#");
+        channel.exchangeDeclare("mumble.dl", FANOUT);
 
+        final Map<String, Object> arguments = new HashMap<>();
+        arguments.put("x-dead-letter-exchange", "mumble.dl");
+        channel.queueDeclare("db", true, false, false, arguments);
+
+        channel.queueDeclare("dlq", true, false, false, null);
+
+        channel.queueBind("db", "mumble", "#");
+        channel.queueBind("dlq", "mumble.dl", "");
+
+        channel.basicQos(1);
         channel.basicConsume("db", false, new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-                messages.add(new Message(new String(body), properties.getHeaders().get("dataOra").toString(),properties.getHeaders().get("mittente").toString(), properties.getHeaders().get("stanza").toString()));
                 try {
+                    messages.add(new Message(new String(body), properties.getHeaders().get("dataOra").toString(), properties.getHeaders().get("mittente").toString(), properties.getHeaders().get("stanza").toString()));
                     sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                } catch (Exception e) {
+                    channel.basicNack(envelope.getDeliveryTag(), false, false);
                 }
-                channel.basicAck(envelope.getDeliveryTag(), false);
+
                 System.out.println(messages);
             }
         });
