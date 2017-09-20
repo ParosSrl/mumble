406b1be796b66144c7c954662f0b88bfb2e16056
Alessandro Dalfovo <alessandro.dalfovo@paros.srl>
2017-09-20 13:14:05 +0200

    Refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,13 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.lang.System.*;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        final BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Chi sei?");
+        final BufferedReader consoleInput = new BufferedReader(new InputStreamReader(in));
+        out.println("Chi sei?");
         String user = consoleInput.readLine();
 
         final Connection connection = Context.init(user);
@@ -21,22 +23,41 @@ public class Main {
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) {
                 final Map<String, Object> headers = properties.getHeaders();
 
-                System.out.println("\t\t[" + headers.get("dataOra") + "] Ricevuto messaggio da " + headers.get("mittente") + " su stanza " +headers.get("stanza") + ": " + new String(body));
+                out.println("\t\t[" + headers.get("dataOra") + "] Ricevuto messaggio da " + headers.get("mittente") + " su stanza " +headers.get("stanza") + ": " + new String(body));
             }
         });
 
         final Channel producer = connection.createChannel();
-        while (true) {
-            System.out.println("Su che stanza vuoi mandare il messaggio? (Digita 'exit' per uscire)");
-            String stanza = consoleInput.readLine();
-            if ("exit".equals(stanza)) break;
+        boolean exit = false;
+        while (!exit) {
+            out.println("Operazione da eseguire:\n\t1) Messaggio privato;\n\t2) Messaggio stanza;\n\t3) exit;");
+            final String command = consoleInput.readLine();
+            String destination, message;
 
-            System.out.println("Digita il messaggio da inviare e premi invio!");
-            String message = consoleInput.readLine();
-            System.out.println("Sei stata bravissima!");
+            switch (command) {
+                case "1":
+                    out.println("A chi vuoi mandare il messaggio? ");
+                    destination = consoleInput.readLine();
+                    out.println("Digita il messaggio da inviare e premi invio!");
+                    message = consoleInput.readLine();
 
-            producer.basicPublish("mumble", "stanze." + stanza, properties(stanza, user), message.getBytes());
-            System.out.println("Molto bravissima, il tuo messaggio del tuo cuore è stato forse inviato!");
+                    producer.basicPublish("mumble", "utenti." + destination, properties(destination, user), message.getBytes());
+                    break;
+
+                case "2":
+                    out.println("Su che stanza vuoi mandare il messaggio? ");
+                    destination = consoleInput.readLine();
+                    out.println("Digita il messaggio da inviare e premi invio!");
+                    message = consoleInput.readLine();
+                    producer.basicPublish("mumble", "stanze." + destination, properties(destination, user), message.getBytes());
+                    break;
+
+                default:
+                    exit = true;
+                    break;
+            }
+
+            out.println("Sei stata bravissima!");
         }
 
         consoleInput.close();
@@ -44,7 +65,7 @@ public class Main {
     }
 
     private static AMQP.BasicProperties properties(String stanza, String mittente) {
-        HashMap<String, Object> headers = new HashMap<String, Object>();
+        Map<String, Object> headers = new HashMap<>();
         headers.put("stanza", stanza);
         headers.put("dataOra", LocalDateTime.now().toString());
         headers.put("mittente", mittente);
