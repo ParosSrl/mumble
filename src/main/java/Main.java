6452e3321c0f3d825a0d0e167cd6480a6de10ca4
Alessandro Dalfovo <alessandro.dalfovo@paros.srl>
2017-09-20 11:50:26 +0200

    add headers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,9 @@ import com.rabbitmq.client.*;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
-import static com.rabbitmq.client.MessageProperties.PERSISTENT_BASIC;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
@@ -15,10 +16,12 @@ public class Main {
         final Connection connection = Context.init(user);
         final Channel consumer = connection.createChannel();
 
-        consumer.basicConsume("stanze."+user, true, new DefaultConsumer(consumer) {
+        consumer.basicConsume("stanze." + user, true, new DefaultConsumer(consumer) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) {
-                System.out.println("\t\tRicevuto messaggio: " +  new String(body));
+                final Map<String, Object> headers = properties.getHeaders();
+
+                System.out.println("\t\t[" + headers.get("dataOra") + "] Ricevuto messaggio da " + headers.get("mittente") + " su stanza " +headers.get("stanza") + ": " + new String(body));
             }
         });
 
@@ -32,12 +35,20 @@ public class Main {
             String message = consoleInput.readLine();
             System.out.println("Sei stata bravissima!");
 
-            producer.basicPublish("mumble", "stanze." + stanza, PERSISTENT_BASIC, message.getBytes());
+            producer.basicPublish("mumble", "stanze." + stanza, properties(stanza, user), message.getBytes());
             System.out.println("Molto bravissima, il tuo messaggio del tuo cuore è stato forse inviato!");
         }
 
         consoleInput.close();
         connection.close();
+    }
+
+    private static AMQP.BasicProperties properties(String stanza, String mittente) {
+        HashMap<String, Object> headers = new HashMap<String, Object>();
+        headers.put("stanza", stanza);
+        headers.put("dataOra", LocalDateTime.now().toString());
+        headers.put("mittente", mittente);
+        return new AMQP.BasicProperties.Builder().appId("mumble").deliveryMode(2).headers(headers).build();
     }
 
 
